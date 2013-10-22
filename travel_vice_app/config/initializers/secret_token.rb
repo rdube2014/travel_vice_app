081f7b4d8fecadf94c2c47e3c8a6882c51399f95
Richard J Dube <rdube2014@kellogg.northwestern.edu>
2013-10-22 18:11:59 -0500

    Created App

---
+++ travel_vice_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TravelViceApp::Application.config.secret_key_base = 'fcf2b5034b72edf3478931870e8b189ad846b3a58dd2f5744aad48c328ce2430fb3abcdb4e87fd8ab69636e47bb8afc3f1e48e65777cab1d447d0a6bac955134'
